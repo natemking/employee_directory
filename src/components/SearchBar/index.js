82b9e7f7ea08193301bc8aefd2d58bce9b32b178
Nate M King <72567521+natemking@users.noreply.github.com>
2021-02-02 15:51:38 -0500

    Updated manifest for new icons

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return null;
+}
+
+export default SearchBar;
---
+++ src/components/Table/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Table = () => {
+  return <p>test</p>
+}
+
+export default Table;
