1763a1d63c5f6ec3a52d32d394f8d4ebe13f34bb
Nate M King <72567521+natemking@users.noreply.github.com>
2021-02-02 15:32:00 -0500

    Setup UI component folders and files. Imported them into app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Header from './components/Header';
-
+import Wrapper from './components/Wrapper';
+import SearchBar from './components/SearchBar';
+import Table from './components/Table';
+ 
 
 function App() {
   return (
     <>
       <Header />
+      <Wrapper>
+        <SearchBar />
+        <Table />
+      </Wrapper>
+      
     </>
   )
 }
